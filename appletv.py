fc87968618e4580aa7a1186a1d72af526bee1a43
Abid Kidwai <abidkidwai786@gmail.com>
2024-03-14 03:53:24 +0530

    First Commit

--- appletv.py
+++ appletv.py
@@ -11,7 +11,7 @@ def getCaps():
         "isRealMobile":True,
         "build": "Apple TV Testing",
         "video": True,
-        "app":"APP_URL",     #Enter app url here
+        "app":"lt://APP1016043281709895160817365",     #Enter app url here
         "network": False,
         "geoLocation": "FR",
         "devicelog": True,
@@ -33,60 +33,66 @@ def runTest():
         accesskey = os.environ.get("LT_ACCESS_KEY")
 
     # grid url
-    gridUrl = "mobile-hub-internal.lambdatest.com/wd/hub"
+    gridUrl = "mobile-hub.lambdatest.com/wd/hub"
 
     # capabilities
     desired_cap = getCaps()
     url = "https://"+username+":"+accesskey+"@"+gridUrl
 
-    print("Initiating remote driver on platform: "+desired_cap["deviceName"]+" browser: "+" version: "+desired_cap["platformVersion"])
-    driver = webdriver.Remote(
-        desired_capabilities=desired_cap,
-        command_executor= url
-    )
+    try:
+        print("Initiating remote driver on platform: "+desired_cap["deviceName"]+" browser: "+" version: "+desired_cap["platformVersion"])
+        driver = webdriver.Remote(
+            desired_capabilities=desired_cap,
+            command_executor= url
+        )
 
-    # run test
-    print(driver.session_id)
+        # run test
+        print(driver.session_id)
 
-    element = driver.switch_to.active_element
-    active = element.get_attribute('label')
+        element = driver.switch_to.active_element
+        active = element.get_attribute('label')
 
-    if active != "Keyboard":
-      element = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="EnterWebsiteName")
-      element.click()
-      time.sleep(1) 
+        if active != "Keyboard":
+          element = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="EnterWebsiteName")
+          element.click()
+          time.sleep(1) 
 
-    element = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="EnterWebsiteName")
-    element.clear()
-    time.sleep(1)   
+        element = driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="EnterWebsiteName")
+        element.clear()
+        time.sleep(1)   
 
-    element = driver.find_element(by=AppiumBy.CLASS_NAME, value="//XCUIElementTypeApplication[@name=\"chief.qaTvTestApp\"]/XCUIElementTypeWindow/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[3]/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeTextView/XCUIElementTypeOther/XCUIElementTypeOther")
-    element.send_keys("https://ifconfig.me")    
+        element = driver.find_element(by=AppiumBy.CLASS_NAME, value="//XCUIElementTypeApplication[@name=\"chief.qaTvTestApp\"]/XCUIElementTypeWindow/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[3]/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeOther/XCUIElementTypeOther[1]/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther[2]/XCUIElementTypeTextView/XCUIElementTypeOther/XCUIElementTypeOther")
+        element.send_keys("https://ifconfig.me")    
 
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Down' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Down' })
-    driver.execute_script('mobile: pressButton', { 'name': 'Down' })    
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Down' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Down' })
+        driver.execute_script('mobile: pressButton', { 'name': 'Down' })    
 
-    element = driver.switch_to.active_element
-    element.click()
-    time.sleep(1)   
+        element = driver.switch_to.active_element
+        element.click()
+        time.sleep(1)   
 
-    driver.execute_script('mobile: pressButton', { 'name': 'Right' })
-    element = driver.switch_to.active_element
-    element.click()
-    time.sleep(3)   
+        driver.execute_script('mobile: pressButton', { 'name': 'Right' })
+        element = driver.switch_to.active_element
+        element.click()
+        time.sleep(3)   
+        driver.execute_script("lambda-status=passed")
 
-    driver.quit()   
+        driver.quit()  
+    except Exception as e:
+        print("An error occurred:", str(e))
+        driver.execute_script("lambda-status=failed")
+        driver.quit()
 
 if __name__ == "__main__":
     runTest()
